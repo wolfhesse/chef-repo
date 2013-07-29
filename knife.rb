e77dc3790fb68231c77f08fc4d507535b1d4256e
wolfhesse <wolfgang.schuessel@gmail.com>
2013-07-29 09:43:40 +0200

    wp

--- knife.rb
+++ knife.rb
@@ -2,10 +2,10 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "wolfhesse"
-client_key               "#{current_dir}/wolfhesse.pem"
+client_key               "#{current_dir}/.chef/wolfhesse.pem"
 validation_client_name   "fruehlingsrolle-validator"
-validation_key           "#{current_dir}/fruehlingsrolle-validator.pem"
+validation_key           "#{current_dir}/.chef/fruehlingsrolle-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/fruehlingsrolle"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
-cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_path            ["#{current_dir}/cookbooks"]
