cb16f29e9f209a3108612f9ebef7f89d7949204e
wolfhesse <wolfgang.schuessel@gmail.com>
2013-07-29 09:29:06 +0200

    wp

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "wolfhesse"
+client_key               "#{current_dir}/wolfhesse.pem"
+validation_client_name   "fruehlingsrolle-validator"
+validation_key           "#{current_dir}/fruehlingsrolle-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/fruehlingsrolle"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
